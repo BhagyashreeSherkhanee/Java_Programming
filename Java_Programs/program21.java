b6d2991c2a1cf82729b99899e7d33bbaa846b8ab
BhagyashreeSherkhanee <bhagyashreesherkhanee@gmail.com>
2025-11-21 12:30:14 +0530

    Add files via upload

---
+++ Java_Programs/program21.java
@@ -0,0 +1,22 @@
+//Best Approach
+
+class Marvellous
+{
+    public void Display()
+    {
+        System.out.println("Jay Ganesh...");
+        System.out.println("Jay Ganesh...");
+        System.out.println("Jay Ganesh...");
+        System.out.println("Jay Ganesh...");
+    }
+}
+
+class program21
+{
+    public static void main(String a[])
+    {
+        Marvellous mobj = new Marvellous();
+        
+        mobj.Display();               
+    }
+}
